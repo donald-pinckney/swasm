94f58e231bcd1bee9a72661a75a1db3fa14caa9c
Donald Pinckney <donald_pinckney@icloud.com>
2019-11-26 13:13:32 -0500

    Remove linux files???

--- Tests/LinuxMain.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-////import swasmTests
-//import parserTests
-//
-//var tests = [XCTestCaseEntry]()
-////tests += swasmTests.allTests()
-//tests += parserTests.allTests()
-//XCTMain(tests)
